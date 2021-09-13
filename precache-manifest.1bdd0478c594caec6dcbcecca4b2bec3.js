e5e79b987b219ae7297c3520de1414d9153ce2e1
Joel <oldmate_joel@hotmail.com>
2021-09-13 22:55:43 +1000

    Updates

---
+++ precache-manifest.1bdd0478c594caec6dcbcecca4b2bec3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f091e636fbc210005f990f3626166d8",
+    "url": "/SolidityMintableProject/index.html"
+  },
+  {
+    "revision": "38aeb95d38addba0519c",
+    "url": "/SolidityMintableProject/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "afddbe45718c3596d017",
+    "url": "/SolidityMintableProject/static/js/2.7faedc76.chunk.js"
+  },
+  {
+    "revision": "38aeb95d38addba0519c",
+    "url": "/SolidityMintableProject/static/js/main.5be30acc.chunk.js"
+  },
+  {
+    "revision": "778056e4241c9e3484fa",
+    "url": "/SolidityMintableProject/static/js/runtime-main.dfbd5fc1.js"
+  }
+]);
